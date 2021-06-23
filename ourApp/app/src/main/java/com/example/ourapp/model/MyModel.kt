9c591e0a130fb458e7067d0f5976d5a036bafa3a
DarkLe3der <amnb9@walla.com>
2021-06-23 21:04:24 +0300

    Amit - managed to connect to fg -> need to implement throttle seekBar and joystick logic

--- ourApp/app/src/main/java/com/example/ourapp/model/MyModel.kt
+++ ourApp/app/src/main/java/com/example/ourapp/model/MyModel.kt
@@ -15,33 +15,32 @@ class MyModel {
     var aileron: Float = 0f
     var elevator: Float = 0f
     var fps: Int = 60
+    private var socket: Socket? = null
 
     private var connected: Boolean = false
 
-     fun connectToServer(): Boolean {
-        try {
-            println("Inside model: ip is $ip")
-            println("Inside model: port is $port")
-            val socket = Socket(ip, port)
-            val outputStream: OutputStream = socket.getOutputStream()
-            connected = true
-            println("Connected to $ip, $port")
-            thread { run(outputStream) }
-        } catch (e: Exception) {
-            println("FAILED TO CONNECT")
-            return false
-        }
-         return true
-
+     fun connectToServer() {
+         thread { run() }
     }
 
-    private fun run(outputStream: OutputStream) {
-        while (connected) {
-            write("set /controls/flight/aileron $aileron\r\n", outputStream)
-            write("set /controls/flight/elevator $elevator\r\n", outputStream)
-            write("set /controls/flight/rudder $rudder\r\n", outputStream)
-            write("set /controls/flight/current-engine/throttle $throttle\r\n", outputStream)
-            Thread.sleep((1000 / fps).toLong())
+    private fun run() {
+        println("Inside Thread")
+        try {
+            socket = Socket(ip, port)
+            var outputStream: OutputStream = socket!!.getOutputStream()
+            connected = true
+            println("CONNECTED TO $ip, $port")
+            while (connected) {
+                write("set /controls/flight/aileron $aileron\r\n", outputStream)
+                write("set /controls/flight/elevator $elevator\r\n", outputStream)
+                write("set /controls/flight/rudder $rudder\r\n", outputStream)
+                write("set /controls/flight/current-engine/throttle $throttle\r\n", outputStream)
+                Thread.sleep((1000 / fps).toLong())
+            }
+        } catch (e: Exception) {
+            println("ERROR")
+            println(e.message)
+            return
         }
     }
 
